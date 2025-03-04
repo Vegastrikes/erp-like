419f2326121d38a8d864a40718edf1c8497dd8ac
Ozerk Ozay <ozerkozay@hotmail.com>
2025-03-04 12:37:58 +0300

    add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import LoginPage from './pages/login'
 
 function App() {
 
   return (
     <div>
-      Hello App
+      <LoginPage />
     </div>
   )
 }
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,16 @@
+export default function LoginPage() {
+    return (
+        <Login />
+    )
+}
+
+function Login() {
+    return (
+        <div className="">
+            <label>Username</label>
+            <input type="text"></input>
+            <label>Password</label>
+            <input type="text"></input>
+        </div>
+    )
+}
